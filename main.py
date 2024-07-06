00d67385c8c77e803aae9de5bdf03bb6ff416af9
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2024-07-07 08:46:51 +0900

    update

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
 import discord
+from os.path import join, dirname
+from dotenv import load_dotenv
+import time
+import os
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client=client)
+
